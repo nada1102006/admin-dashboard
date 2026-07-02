09622b252ed27c6c0c74ebc50c4fb577ea7dc8ec
Nada Yasser <ny1237@fayoum.edu.eg>
2026-07-02 12:51:44 +0300

    feat: setup project structure and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+import Login from './pages/Login';
+import DashboardHome from './pages/DashboardHome';
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
+import AddProduct from './pages/AddProduct';
+import Orders from './pages/Orders';
+import Users from './pages/Users';
+import Carts from './pages/Carts';  
+import Settings from './pages/Setting';
+
+
 function App() {
   return (
-    <>
-      <h1>Admin Dashboard</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<DashboardHome />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="/add-product" element={<AddProduct />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/carts" element={<Carts />} />
+        <Route path="/settings" element={<Settings />} />
+
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
+
+      </Routes>
+      </Router>
   );
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,2 @@
+import React from 'react';
+export default function Login() { return <div>Yassmin Task</div>; }
