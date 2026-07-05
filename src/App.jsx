9b5ac4264369c3fea52852d749be934a9659f351
Nesma-Ramadan <nesmaramadan2019@gmail.com>
2026-07-05 15:29:53 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,71 @@
-import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from "./pages/Login";
+import DashboardHome from "./pages/DashboardHome";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
+import AddProduct from "./pages/AddProduct";
+import Orders from "./pages/Orders";
+import Users from "./pages/Users";
+import Carts from "./pages/Carts";
+import Settings from "./pages/Setting";
+import Layout from "./Layout/Layout";
 
-import Login from './pages/Login';
-import DashboardHome from './pages/DashboardHome';
-import Products from './pages/Products';
-import ProductDetails from './pages/ProductDetails';
-import AddProduct from './pages/AddProduct';
-import Orders from './pages/Orders';
-import Users from './pages/Users';
-import Carts from './pages/Carts';  
-import Settings from './pages/Setting';
-
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "dashboard",
+        element: <DashboardHome />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "products/:id",
+        element: <ProductDetails />,
+      },
+      {
+        path: "add-product",
+        element: <AddProduct />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+      {
+        path: "users",
+        element: <Users />,
+      },
+      {
+        path: "carts",
+        element: <Carts />,
+      },
+      {
+        path: "settings",
+        element: <Settings />,
+      },
+      {
+        path: "*",
+        element: <Navigate to="/dashboard" replace />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<DashboardHome />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="/add-product" element={<AddProduct />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/carts" element={<Carts />} />
-        <Route path="/settings" element={<Settings />} />
-
-        <Route path="*" element={<Navigate to="/dashboard" replace />} />
-
-      </Routes>
-      </Router>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
