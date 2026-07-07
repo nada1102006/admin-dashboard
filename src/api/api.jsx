278d3a30bf37676b24673585580b73bb115e97df
shahdahmed08 <shahdahmed0801@gmail.com>
2026-07-07 22:52:33 +0300

    dashboard&best seller

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,25 +1,16 @@
 import axios from "axios";
 
-// Create an Axios instance with a base URL
 const api = axios.create({
-  baseURL: "https://e-commerce-api-3wara.vercel.app/",
-})
+  baseURL: "https://e-commerce-api-3wara.vercel.app",
+});
 
-// Add an interceptor to the Axios instance
-api.interceptors.request.use(
-  (config) => {
-    // Get the user's token from local storage
-    const token = localStorage.getItem("userToken")
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token"); 
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
+  return config;
+});
 
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error)
-  },
-)
-
-export default api
+export default api;
--- src/pages/DashboardHome.jsx
+++ src/pages/DashboardHome.jsx
@@ -1,4 +1,135 @@
-import React from 'react';
-export default function Login() { return <div>
-    Shahd Task  && Arwa Task  && Ranim Task
-</div>; }
+import { useEffect, useState } from "react";
+import api from "../api/api";
+import "../styles/Dashboard.css";
+import {
+  FaShoppingBag,
+  FaClock,
+  FaDollarSign,
+  FaChartLine,
+  FaUsers,
+  FaBoxOpen,
+} from "react-icons/fa";
+export default function DashboardHome() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getDashboard = async () => {
+      try {
+        const res = await api.get("/orders/admin/dashboard");
+        setData(res.data.dashboard);
+      } catch (err) {
+        console.log("Dashboard Error:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getDashboard();
+  }, []);
+
+  if (loading) {
+    return <h2 className="loading">Loading...</h2>;
+  }
+
+const cards = [
+  {
+    title: "Total Orders",
+    value: data?.orders?.total || 0,
+    icon: <FaShoppingBag />,
+    color: "#4F46E5",
+    bg: "#EEF2FF",
+  },
+  {
+    title: "Pending Orders",
+    value: data?.orders?.pending || 0,
+    icon: <FaClock />,
+    color: "#F59E0B",
+    bg: "#FEF3C7",
+  },
+  {
+    title: "Revenue",
+    value: `$${data?.revenue?.total || 0}`,
+    icon: <FaDollarSign />,
+    color: "#10B981",
+    bg: "#D1FAE5",
+  },
+  {
+    title: "This Month",
+    value: `$${data?.revenue?.lastMonth || 0}`,
+    icon: <FaChartLine />,
+    color: "#EC4899",
+    bg: "#FCE7F3",
+  },
+  {
+    title: "Users",
+    value: data?.totalCustomers || 0 ,
+    icon: <FaUsers />,
+    color: "#3B82F6",
+    bg: "#DBEAFE",
+  },
+  {
+    title: "Top Product",
+    value: data?.topProducts?.[0]?.totalSold || 0,
+    icon: <FaBoxOpen />,
+    color: "#8B5CF6",
+    bg: "#EDE9FE",
+  },
+];
+
+return (
+  <section className="dashboard">
+
+    <div className="dashboard-header">
+      <h1>Admin Overview</h1>
+      <p>Monitor your store performance.</p>
+    </div>
+
+    <div className="cards-grid">
+  {cards.map((card, index) => (
+   <div
+  className="card"
+  key={index}
+  style={{ background: card.bg }}
+>
+      <div
+        className="card-icon"
+        style={{ background: card.color }}
+      >
+        {card.icon}
+      </div>
+
+      <h4>{card.title}</h4>
+
+      <h2>{card.value}</h2>
+    </div>
+  ))}
+</div>
+
+    <div className="bottom-section">
+   <div className="best-seller">
+
+  <h3>Best Seller</h3>
+
+  {data?.topProducts?.map((product, index) => (
+    <div className="product-item" key={index}>
+
+      <img
+        src={product.image}
+        alt={product.name}
+      />
+
+      <div>
+        <h4>{product.name}</h4>
+        <p>{product.totalSold} Sold</p>
+      </div>
+
+    </div>
+  ))}
+
+</div>
+
+    </div>
+
+  </section>
+)};
