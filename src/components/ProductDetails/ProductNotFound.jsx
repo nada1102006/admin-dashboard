a1f102579006cccb0718130937c43ebe86cf0a60
Nesma-Ramadan <nesmaramadan2019@gmail.com>
2026-07-14 15:33:06 +0300

    n

---
+++ src/components/ProductDetails/ProductNotFound.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+
+export default function ProductNotFound() {
+  return (
+    <>
+     <div className="flex min-h-screen flex-col items-center justify-center bg-slate-100 dark:bg-slate-950">
+        <h2 className="text-3xl font-bold text-slate-800 dark:text-white">
+          Product Not Found
+        </h2>
+
+        <Link
+          to="/products"
+          className="mt-6 rounded-xl bg-slate-900 px-6 py-3 text-white transition hover:bg-slate-800"
+        >
+          Back to Products
+        </Link>
+      </div>
+    </>
+  )
+}
